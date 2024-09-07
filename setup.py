a378cfae9dfa32320653b80344b080af5bcf5d4a
Denny Baldini <dennybaldini@gmail.com>
2024-09-07 20:58:52 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='meteonetwork-api-client-python',
-    version='0.1.0',
-    description='Simple python client for meteonetwork api',
-    author='Denny Baldini',
-    author_email='dennybaldini@gmail.com',
-    url='https://github.com/dennyb87/meteonetwork-api-client-python',
-    packages=['requests==2.31.0'],
+    name="meteonetwork-api-client-python",
+    version="0.1.0",
+    description="Simple python client for meteonetwork api",
+    author="Denny Baldini",
+    author_email="dennybaldini@gmail.com",
+    url="https://github.com/dennyb87/meteonetwork-api-client-python",
+    install_requires=[
+        "requests==2.31.0",
+    ],
 )
